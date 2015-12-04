8b4c611179a055f10f68446cde67fe103379075d
ArttuHuusko <arttu.huus@gmail.com>
2015-12-04 11:38:59 +0200

    :+1: :cat:
    joooooooooo

---
+++ Assets/scripts/customerArrive.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class customerArrive : MonoBehaviour {
+
+	Vector3 startPos;
+	// Use this for initialization
+	void Start () {
+		startPos = transform.position;
+		Debug.Log (startPos);
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
