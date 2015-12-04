2b6923c3fa221f9c334054fc733f492591c255d6
Aapo Ritvanen <aapo.ritvanen@gmail.com>
2015-12-04 10:29:05 +0200

    Unity projektin pushaaminen
    :-DDD

---
+++ Assets/Draggable.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+public class Draggable : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler {
+	
+	private Vector3 startPos;
+
+	void Start()
+	{
+		print (transform.position);
+		startPos = transform.position;
+	}
+
+	public void OnBeginDrag (PointerEventData eventData)
+	{
+		Debug.Log ("OnBegindDrag");
+	}
+	public void OnDrag(PointerEventData eventData)
+	{
+		this.transform.position = eventData.position;
+		Debug.Log ("OnDrag");
+		this.GetComponent<Image> ().color = Color.white;
+
+	}
+	public void OnEndDrag(PointerEventData eventData)
+	{
+		Debug.Log ("OnEndDrag");
+		this.transform.position = startPos;
+		this.GetComponent<Image> ().color = Color.clear;
+
+	}
+
+
+}
