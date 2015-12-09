10b8536dcd5d56eebfd45e29bd50dbe2bfb6182a
JoonaJappinen <jappinen.joona@gmail.com>
2015-12-09 15:59:35 +0200

    Menu logic
    äää ööö aaa

---
+++ Assets/scripts/menuScript.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class menuScript: MonoBehaviour 
+{
+	public GameObject pauseMenu;
+
+	private bool paused = false;
+
+	void Start()
+	{
+		pauseMenu.SetActive(false);
+	}
+
+	void Update()
+	{
+		if(Input.GetKeyDown(KeyCode.P) || Input.GetKeyDown(KeyCode.Escape))
+		{
+			paused = !paused;
+		}
+
+		if(paused)
+		{
+			pauseMenu.SetActive(true);
+			Time.timeScale = 0;
+		}
+
+		if(!paused)
+		{
+			pauseMenu.SetActive(false);
+			Time.timeScale = 1;
+		}
+	}
+
+	public void Resume()
+	{
+		paused = false;
+	}
+
+	public void Play()
+	{
+		Application.LoadLevel ("pauseMenu");
+	}
+
+	public void backToM()
+	{
+		Application.LoadLevel ("mainMenu");
+	}
+
+}
