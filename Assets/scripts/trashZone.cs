387fba706d9fc97d9e18e4d800464b84919314eb
Apetzu <aapo.ritvanen@gmail.com>
2015-12-17 15:41:48 +0200

    Trash update :P

---
+++ Assets/scripts/trashZone.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+public class trashZone : MonoBehaviour, IDropHandler {
+
+	public GameObject sausageObj;
+	public GameObject saladObj;
+	public GameObject sauceObj;
+	public GameObject draggableBreadObj;
+	plateZone finishedBread;
+	public GameObject doneBread;
+
+	void Start()
+	{
+		finishedBread = doneBread.GetComponent<plateZone> ();
+	}
+
+	public void OnDrop(PointerEventData eventData)
+	{
+		if (eventData.pointerDrag == doneBread)
+		{
+			sausageObj.GetComponent<Image> ().color = Color.clear;
+			saladObj.GetComponent<Image> ().color = Color.clear;
+			sauceObj.GetComponent<Image> ().color = Color.clear;
+			draggableBreadObj.GetComponent<Image> ().color = Color.clear;
+			finishedBread.sauce = false;
+			finishedBread.wiener = false;
+			finishedBread.bread = false;
+			finishedBread.salad = false;
+		}
+	}
+}
