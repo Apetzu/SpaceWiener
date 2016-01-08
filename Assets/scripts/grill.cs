996bfb2d74d0b4f220c594a9b69acf558b535773
Apetzu <aapo.ritvanen@gmail.com>
2016-01-08 13:48:55 +0200

    Grill v0.1 :hamburger:

---
+++ Assets/scripts/grill.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+public class grill : MonoBehaviour, IDropHandler {
+	
+	public void OnDrop(PointerEventData eventData)
+	{
+
+	}
+	
+	public void OnBeginDrag (PointerEventData eventData)
+	{
+
+	}
+	
+	public void OnDrag(PointerEventData eventData)
+	{
+
+	}
+	
+	public void OnEndDrag(PointerEventData eventData)
+	{
+
+	}
+}
