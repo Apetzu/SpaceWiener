8b21ca43ecdc6158af292c6ad9ed4ba042085f65
Apetzu <aapo.ritvanen@gmail.com>
2016-01-13 12:31:44 +0200

    You can put stuff to grill! :P

--- Assets/scripts/grill.cs
+++ Assets/scripts/grill.cs
@@ -5,23 +5,29 @@ using UnityEngine.EventSystems;
 
 public class grill : MonoBehaviour, IDropHandler {
 	
+	public GameObject[] allGrillObjects = new GameObject[4];
+	bool[] takenGrill;
+
+	void Start()
+	{
+		takenGrill = new bool[allGrillObjects.Length];
+	}
+
 	public void OnDrop(PointerEventData eventData)
 	{
-
-	}
-	
-	public void OnBeginDrag (PointerEventData eventData)
-	{
-
-	}
-	
-	public void OnDrag(PointerEventData eventData)
-	{
-
-	}
-	
-	public void OnEndDrag(PointerEventData eventData)
-	{
-
+		if (eventData.pointerDrag.tag == "sausage") 
+		{
+			for (int i = 0; i < takenGrill.Length; i++) 
+			{
+				if (takenGrill[i] == false)
+				{
+					takenGrill[i] = true;
+					allGrillObjects[i].GetComponent<Image>().sprite = eventData.pointerDrag.GetComponent<draggableItem>().onDogSpriteReplace;
+					allGrillObjects[i].GetComponent<Image>().color = Color.white;
+					allGrillObjects[i].GetComponent<CanvasGroup>().blocksRaycasts = true;
+					break;
+				}
+			}
+		}
 	}
 }
---
+++ Assets/scripts/grillItem.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+public class grillItem : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler {
+	
+	private Vector3 startPos;
+	public Sprite onDogSpriteReplace;
+	public Camera mainCamera;
+
+	public void OnBeginDrag (PointerEventData eventData)
+	{
+		startPos = transform.position;
+		GetComponent<CanvasGroup>().blocksRaycasts = false;
+	}
+	public void OnDrag(PointerEventData eventData)
+	{
+		transform.position = mainCamera.ScreenToWorldPoint (eventData.position) + Vector3.forward;
+	}
+	public void OnEndDrag(PointerEventData eventData)
+	{
+		transform.position = startPos;
+		GetComponent<CanvasGroup>().blocksRaycasts = true;
+	}
+}
